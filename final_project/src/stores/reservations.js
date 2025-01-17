9a1627ab54f481e41dc32820f7cbcc35ddb2a940
TomasZsembera <tomas.zsembera@student.ukf.sk>
2025-01-17 18:42:46 +0100

    perzistentne úložisko

--- final_project/src/stores/reservations.js
+++ final_project/src/stores/reservations.js
@@ -2,18 +2,22 @@ import { defineStore } from 'pinia';
 
 export const useReservationsStore = defineStore('reservations', {
   state: () => ({
-    reservations: [],
+    reservations: JSON.parse(localStorage.getItem('reservations') || '[]'),
   }),
   actions: {
     addReservation(reservation) {
       this.reservations.push(reservation);
+      this.saveToLocalStorage();
     },
-
     removeReservation(reservation) {
       const index = this.reservations.indexOf(reservation);
-      this.reservations.splice(index, 1);
+      if (index > -1) {
+        this.reservations.splice(index, 1);
+        this.saveToLocalStorage();
+      }
+    },
+    saveToLocalStorage() {
+      localStorage.setItem('reservations', JSON.stringify(this.reservations));
     },
   },
-
-
 });
