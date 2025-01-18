885a91800b37ac8cbb08153e29e7d8e3dc415baa
TomasZsembera <tomas.zsembera@student.ukf.sk>
2025-01-18 14:21:58 +0100

    css

--- final_project/src/components/goBackComponent.vue
+++ final_project/src/components/goBackComponent.vue
@@ -18,21 +18,51 @@ export default {
 <style scoped>
 .go-back-container {
   position: fixed;
-  top: 100px; 
+  top: 100px; /* Adjust based on your header height */
   left: 10px;
-  z-index: 1000; 
+  z-index: 1000; /* Ensure it stays above other content */
 }
 
 .go-back-button {
-    padding: 10px 15px;
-    font-size: 1rem;
-    background-color: #529e96ad;
-    color: #fff;
-    border: none;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  padding: 10px 15px;
+  font-size: 1rem;
+  background-color: #529e96ad;
+  color: #fff;
+  border: none;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
 }
 
 .go-back-button:hover {
-    background-color: #529e96;
+  background-color: #529e96;
+}
+
+@media (max-width: 768px) {
+  .go-back-container {
+    top: 320px; /* Adjust based on your header height */
+    left: 5px;
+  }
+  .go-back-button {
+  padding: 5px 5px;
+  font-size: 0.8rem;
+  background-color: #529e96ad;
+  color: #fff;
+  border: none;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
+}
+
+@media (max-width: 576px) {
+  .go-back-container {
+    top: 320px; /* Adjust based on your header height */
+    left: 5px;
+  }
+  .go-back-button {
+  padding: 5px 5px;
+  font-size: 0.8rem;
+  background-color: #529e96ad;
+  color: #fff;
+  border: none;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
 }
 </style>
